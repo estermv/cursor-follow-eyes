d84a8f4f895d07b77c6adf43d09b50423f6ddaf7
Ester Martí <ester.m87@gmail.com>
2017-01-30 08:22:00 +0100

    Add movement to both eyes

--- scripts.js
+++ scripts.js
@@ -1,12 +1,24 @@
 'use strict';
 
-var circleX = 78.56;
-var circleY = 69.11;
+var originLeft = {
+  x: 78.56,
+  y: 69.11
+};
+var originRight = {
+  x: 121.44,
+  y: 69.11
+}
+
 var circleRadius = 3.65;
-var leftEye;
+var leftEye, leftEyePosition, rightEye, rightEyePosition;
 
 window.onload = function() {
   leftEye = document.querySelector('#eye-left circle');
+  leftEyePosition = leftEye.getBoundingClientRect();
+
+  rightEye = document.querySelector('#eye-right circle');
+  rightEyePosition = rightEye.getBoundingClientRect();
+
   this.addEventListener('mousemove', moveEyes);
 }
 
@@ -14,17 +26,34 @@ function moveEyes (event) {
   var x = event.pageX,
       y = event.pageY;
 
-    // Get the mouse position relative to the centre of the circle (circleX,circleY)
-    var  dx = x - circleX;
-    var  dy = y - circleY;
+  moveEye(x, y, leftEye, leftEyePosition, originLeft);
+  moveEye(x, y, rightEye, rightEyePosition, originRight);
 
-    // Calculate distance from centre of circle to mouse (Pythagoras' theorem)
-    var distance = Math.sqrt(dx * dx + dy *dy);
-    
-    // Scale the dx,dy coords back so they are on the circumference
-    dx = 68.08 + (dx * circleRadius / distance);
-    dy = 62.64 + (dy * circleRadius / distance);
+}
 
-    leftEye.setAttribute('cx', dx);
-    leftEye.setAttribute('cy', dy);
+function moveEye(x, y, eye, position, origin) {
+  // Get the mouse position relative to the centre of the circle (circleX,circleY)
+  var  dx = x;
+  var  dy = y;
+
+  var svg = document.getElementById('guillem-svg'),
+  NS = svg.getAttribute('xmlns');
+
+
+  var pt = svg.createSVGPoint(), svgP, circle;
+
+  pt.x = dx;
+  pt.y = dy;
+  svgP = pt.matrixTransform(svg.getScreenCTM().inverse());
+
+  // circle = document.createElementNS(NS, 'circle');
+  var distance = Math.sqrt(svgP.x * svgP.x + svgP.y * svgP.y);
+  var finalx = origin.x + (svgP.x * ((20.96 / 2) - circleRadius) / distance);
+  var finaly = origin.y + (svgP.y * ((12.75 / 2) - circleRadius) / distance);
+
+  // var finalx = (circleX + svgP.x);
+  // var finaly = (circleY + svgP.y);
+
+  eye.setAttributeNS(null, 'cx', finalx);
+  eye.setAttributeNS(null, 'cy', finaly);
 }
